e6ffc0cec865f199ab854170aefaa637ba4e9cf3
emansheesha <emansha3eesha3@gmail.com>
2025-03-03 14:06:33 +0200

    dynamic metadata

--- src/app/(shopping)/products/[id]/page.tsx
+++ src/app/(shopping)/products/[id]/page.tsx
@@ -1,8 +1,36 @@
-import { Button } from "@/app/components/Button";
+import type { Metadata, ResolvingMetadata } from "next";
 import { SkeletonProducts } from "@/app/components/SkeletonProducts";
 import Image from "next/image";
 import { Suspense } from "react";
 
+type Props = {
+  params: Promise<{ id: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const { id } = await params;
+
+  // fetch data
+  const product = await fetch(`https://dummyjson.com/products/${id}`).then(
+    (res) => res.json()
+  );
+
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: product.title,
+    description: product.description,
+    // openGraph: {
+    //   images: ['/some-specific-page-image.jpg', ...previousImages],
+    // },
+  };
+}
 // get id  from path in server component
 export default async function ProductId({
   params,
@@ -16,6 +44,7 @@ export default async function ProductId({
       console.error(error);
     });
   console.log("data", data);
+
   return (
     <div>
       <h1>Products Page</h1>
